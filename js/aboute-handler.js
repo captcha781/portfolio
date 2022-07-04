888f06e3d379f60729c8c069b08ba4d689b09359
captcha781 <bhuvanesh19112001@gmail.com>
2022-07-04 20:00:36 +0530

    Skills Section completed

--- js/aboute-handler.js
+++ js/aboute-handler.js
@@ -21,3 +21,26 @@ const activeToggle = (id, des) => {
 
 
 }
+
+const activeToggleSkiller = (id, des) => {
+    let childs = Array(document.getElementById(id).parentElement.children)
+    let polymer = Array(document.getElementById(des).parentElement.children)
+    childs.forEach(element => {
+        for (let i = 0; i<element.length; i++){
+            element[i].classList.remove("bg-LightPurple");
+            element[i].classList.add("bg-slate-700");
+            
+            if (polymer[0][i].classList.contains("hidden")){
+                continue
+            }
+            polymer[0][i].classList.add("hidden")
+            
+        }
+    });
+    document.getElementById(id).classList.add("bg-LightPurple")
+    document.getElementById(id).classList.remove("bg-slate-700")
+    document.getElementById(des).classList.add("block")
+    document.getElementById(des).classList.remove("hidden")
+
+
+}
